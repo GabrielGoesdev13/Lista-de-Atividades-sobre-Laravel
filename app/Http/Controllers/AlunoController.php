44298f9465e6fe01ada951a9d7bf6401b2d2806e
GabrielGoesdev13 <engs-gabrielgoes@camporeal.edu.br>
2026-09-08 19:01:50 -0300

    ATV 13: Implementar CRUD completo no AlunoController

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aluno;
+use Illuminate\Http\Request;
+
+class AlunoController extends Controller
+{
+    public function index()
+    {
+        $alunos = Aluno::all();
+        return view('alunos.index', compact('alunos'));
+    }
+
+    public function show($id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        return view('alunos.show', compact('aluno'));
+    }
+
+    public function create()
+    {
+        return view('alunos.create');
+    }
+
+    public function store(Request $request)
+    {
+        Aluno::create($request->all());
+        return redirect()->route('alunos.index');
+    }
+
+    public function edit($id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        return view('alunos.edit', compact('aluno'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        $aluno->update($request->all());
+        return redirect()->route('alunos.index');
+    }
+
+    public function destroy($id)
+    {
+        $aluno = Aluno::findOrFail($id);
+        $aluno->delete();
+        return redirect()->route('alunos.index');
+    }
+}
