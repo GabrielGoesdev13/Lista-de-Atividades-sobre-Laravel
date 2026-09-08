58398cbc2837931c297e0530af3411107d58f0ee
GabrielGoesdev13 <engs-gabrielgoes@camporeal.edu.br>
2026-09-08 18:56:13 -0300

    ATV 8: Criar página home.blade.php usando o layout

---
+++ resources/views/layouts/resources/views/resources/views/alunos/resources/views/alunos/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Detalhes do Aluno')
+
+@section('content')
+    <h2>Detalhes do Aluno</h2>
+    <p>Aqui serão exibidos os dados de um aluno específico.</p>
+@endsection
