4ac13df351dd829b9d9a7c643243d0d09771a2bf
GabrielGoesdev13 <engs-gabrielgoes@camporeal.edu.br>
2026-09-08 18:45:32 -0300

    ATV 1: Criar rotas /sobre, /alunos e /contato

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/sobre', function () {
+    return 'Esta é a página Sobre.';
+});
+
+Route::get('/alunos', function () {
+    return 'Esta é a página de Alunos.';
+});
+
+Route::get('/contato', function () {
+    return 'Esta é a página de Contato.';
+});
+
