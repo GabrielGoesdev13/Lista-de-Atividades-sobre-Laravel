d5fe4673d20c66ae3391dd660ec647b451beaff1
GabrielGoesdev13 <engs-gabrielgoes@camporeal.edu.br>
2026-09-08 18:54:29 -0300

    ATV 7: Criar layout app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Sistema de Alunos')</title>
+</head>
+<body>
+
+    <header>
+        <h1>Sistema de Alunos</h1>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <p>Trabalho de Laravel - Sistema de Alunos</p>
+    </footer>
+
+</body>
+</html>
