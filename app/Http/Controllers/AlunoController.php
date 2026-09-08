b1d1cff588b89cac755cef3ca1c02c960170ff74
GabrielGoesdev13 <engs-gabrielgoes@camporeal.edu.br>
2026-09-08 18:49:33 -0300

    ATV 4: Implementar as 7 rotas principais de CRUD no AlunoController

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Lista de todos os alunos.';
+    }
+
+    public function show($id)
+    {
+        return 'Detalhes do aluno de ID: ' . $id;
+    }
+
+    public function create()
+    {
+        return 'Formulário de cadastro de aluno.';
+    }
+   
+    public function store(Request $request)
+    {
+        return 'Aluno cadastrado com sucesso.';
+    }
+ 
+    public function edit($id)
+    {
+        return 'Formulário de edição do aluno de ID: ' . $id;
+    }
+ 
+    public function update(Request $request, $id)
+    {
+        return 'Aluno de ID ' . $id . ' atualizado com sucesso.';
+    }
+ 
+    public function destroy($id)
+    {
+        return 'Aluno de ID ' . $id . ' removido com sucesso.';
+    }
 }
